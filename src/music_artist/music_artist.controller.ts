8fd710a0ecdbf0b31da94a7567a1c295c1dd21d0
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-05-01 17:52:23 +0900

    라우터 순서 변경 , '' -> '/all'

--- src/music_artist/music_artist.controller.ts
+++ src/music_artist/music_artist.controller.ts
@@ -6,24 +6,19 @@ import { CreateMusicArtistDto } from './dto/create-music_artist.dto';
 export class MusicArtistController {
   constructor(private readonly musicArtistService: MusicArtistService) {}
 
-  @Post()
-  create(@Body() createMusicArtistDto: CreateMusicArtistDto) {
-    try{
-      return this.musicArtistService.create(createMusicArtistDto);
-    }catch(e){
-      console.log(typeof e)
-      return [];
-    }    
-  }
-
-  @Get()
+  @Get('all')
   getAll() {
     return this.musicArtistService.getAll();
   }
 
-  @Get('/find')
+  @Get()
   findById(@Query('music_id') music_id?: string , @Query('artist_id') artist_id?: string){
     return this.musicArtistService.findById(+music_id,+artist_id);
+  }
+
+  @Post()
+  create(@Body() createMusicArtistDto: CreateMusicArtistDto) {
+    return this.musicArtistService.create(createMusicArtistDto);  
   }
 
   @Delete()
